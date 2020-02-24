9ed259775e5f3157f2a1aeedc0cb5f531df5a599
Ludovic Barman <ludovic.barman@protonmail.com>
2020-02-24 17:22:53 +0100

    Changed files simul.sh

--- simul.sh
+++ simul.sh
@@ -134,7 +134,7 @@ case $1 in
 
     simul|Simul|SIMUL)
 
-
+        "$THIS_SCRIPT" simul-cl
 
         EXPERIMENT_ID_VALUE=$(LC_ALL=C cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
         dbg_lvl=1 # do not change this. Many other functions of this script call this script recursively. If this is >1, the log will blow up ;)
